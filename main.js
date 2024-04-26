1487b8c6b7668ba60201f11bc07f8bdecbaaa2bc
vieirazyyy <132680194+vieirazyyy@users.noreply.github.com>
2024-04-26 08:46:13 -0300

    v

--- main.js
+++ main.js
@@ -19,10 +19,10 @@ for(let i=0;i <botoes.length;i++){
 
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-04-30T00:00:00");
-const tempoObjetivo2 = new Date("2024-06-13T14:30:00");
-const tempoObjetivo3 = new Date("2024-12-30T13:20:00");
-const tempoObjetivo4 = new Date("2025-02-01T18:00:00");
+const tempoObjetivo1 = new Date("2024-07-27T00:00:00");
+const tempoObjetivo2 = new Date("2024-10-11T14:30:00");
+const tempoObjetivo3 = new Date("2024-12-24T13:20:00");
+const tempoObjetivo4 = new Date("2024-11-06T18:00:00");
 
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
